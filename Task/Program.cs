87135c7888cd3370ecc27cc4ba57a55d5acbdd28
Anastasiia Bedareva <beda2049@gmail.com>
2022-06-23 13:22:06 +0300

    task completed

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿int numberA;
+int numberB;
+int min;
+int max;
+
+Console.WriteLine("Уважаемый, введите пожалуйста число ");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("А можете ввести ещё одно число? ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+}
+else
+{
+    min = numberA;
+    max = numberB;
+
+}
+
+Console.WriteLine("min =" + min);
+Console.WriteLine("max =" + max);
+
+
